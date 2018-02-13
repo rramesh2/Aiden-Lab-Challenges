2b7112004a80f6d9335e7b9c001731530240d14a
sxxgrc <sg62@rice.edu>
2018-02-12 21:33:14 -0600

    stuff2

---
+++ Number to Word Conversion Challenge/Readme.txt
@@ -0,0 +1,9 @@
+-----Read Me for num2wordmap.py----------
+
+Runs on tkinter (for gui) and written fro python 2.7
+
+
+-----Guide----
+Maps any number to a set of interconnected words with the words' mapping being centered on the isogram subdermatoglyphic, with the index of each letter being a number from 0-16. Therefore, every inputted number is simply converted to a letter based on this mapping.
+
+Program enables encoding of numbers and decoding of words.
--- Number to Word Conversion Challenge/num2wordmap.py
+++ Number to Word Conversion Challenge/num2wordmap.py
@@ -8,9 +8,28 @@ Aiden Lab Challenge 3
 
 import tkinter
 
-def conv_num():
+def conv_num(num):
+    """
+    Converts the inputted number into a 
+    """
 
 
 
-
-mapp = {0: ""}
+mapp = {0: 's',
+        1: 'u',
+        2: 'b',
+        3: 'd',
+        4: 'e',
+        5: 'r',
+        6: 'm',
+        7: 'a',
+        8: 't',
+        9: 'o',
+        10: 'g',
+        11: 'l',
+        12: 'y',
+        13: 'p',
+        14: 'h',
+        15: 'i',
+        16: 'c'
+        }   
